7a02d1cf23cbfe8ea7d5e38f7ece82fe8e342f09
j-0-3 <38348883+J-0-3@users.noreply.github.com>
2025-05-16 21:20:07 +0100

    deleted ghostty, added dwl and foot config

--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@
 
 mkdir -p ~/.config
 cp -r $(dirname $0)/nvim ~/.config/nvim
+cp -r $(dirname $0)/ghostty ~/.config/ghostty
+cp -r $(dirname $0)/foot ~/.config/foot
+git clone https://aur.archlinux.org/dwl.git ~/.config/dwl
+cp $(dirname $0)/dwl/config.h ~/.config/dwl
+cd ~/.config/dwl
+makepkg -si
